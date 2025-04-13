d1b64b80a33cfa11c12e3a049a4016a5bf3366f0
Cosmin Ciocan <justciocan@gmail.com>
2025-04-13 12:27:44 +0100

    Added APIs for Book Cover and Book Title as result from search by ISBN

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -12,11 +13,35 @@ const db = new pg.Client({
   port: 5432,
 });
 
+let bookTitle = "Title";
+let bookCover =
+  "https://www.citypng.com/public/uploads/preview/png-open-book-with-question-mark-cartoon-illustration-701751694862187kfkjrdyrsu.png";
+let bookReview = "No Reviews Yet";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/page", async (req, res) => {
+  const book = req.body.newBook;
+  try {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${book}`
+    );
+    bookTitle = result.data.items[0].volumeInfo.title;
+    bookCover = `https://covers.openlibrary.org/b/isbn/${book}-L.jpg`;
+    res.render("page.ejs", {
+      title: bookTitle,
+      cover: bookCover,
+      review: bookReview,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
 });
 
 app.listen(port, () => {
